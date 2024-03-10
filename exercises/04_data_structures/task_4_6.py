ec6304d926fd8f34add7a9601e9036d024148435
maiano <maiano@yandex.ru>
2024-03-10 20:41:10 +0100

    feat: problem 4.3-4.6 solved

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -28,3 +28,14 @@ Next-Hop              {}
 Last update           {}
 Outbound Interface    {}
 """
+route = ospf_route.replace(",", " ").replace("[", "").replace("]", "").split()
+
+print(
+    template.format(
+        route[0],
+        route[1],
+        route[3],
+        route[4],
+        route[5],
+    )
+)
